0f55f1dea31db3a651428be1da972d7d74fdc187
tkjtkjtkj <tkj_tkj@yahoo.com>
2017-02-12 18:14:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate(artcileOne));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
