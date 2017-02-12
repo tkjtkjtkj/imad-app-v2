e3690f2ad6f7073d210b68685e9492e7b8d4f795
tkjtkjtkj <tkj_tkj@yahoo.com>
2017-02-12 18:12:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,82 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Janarthanan Kesavan',
+    heading: 'Article One',
+    date: 'Feb 12, 2017',
+    content: 'This is my first article'
+    
+};
+
+var articleTwo = {
+    title: 'Article Two | Janarthanan Kesavan',
+    heading: 'Article Two',
+    date: 'Feb 12, 2017',
+    content: 'This is my second article'
+    
+};
+
+var articleThree = {
+    title: 'Article Three | Janarthanan Kesavan',
+    heading: 'Article Three',
+    date: 'Feb 12, 2017',
+    content: 'This is my Three article'
+    
+};
+
+
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+var htmlTemplate = `{
+    <html>
+<head>
+<title>
+${title}
+</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+    
+</head>
+<body>
+    <div class="container">
+<div>
+<a href="/">Home</a>
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+<p>
+${content}
+
+</p>
+</div>
+</div>
+</body>
+</html>
+    
+    
+}
+`;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(artcileOne));
 });
 
 app.get('/article-two', function (req, res) {
